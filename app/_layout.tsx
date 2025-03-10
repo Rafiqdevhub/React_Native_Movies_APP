786199cd021e077468508be507a508fcf6fa74e9
MUhammad Rafiq <rafiq85825@gmail.com>
2025-03-10 15:12:42 +0500

    Refactor RootLayout to consolidate Stack.Screen options and disable swipe gestures

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,37 +6,12 @@ const RootLayout = () => {
   return (
     <>
       <StatusBar hidden={true} />
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="movie/[id]"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="watchlist"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="ratings"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="search-history"
-          options={{
-            headerShown: false,
-          }}
-        />
+      <Stack screenOptions={{
+        headerShown: false,
+        gestureEnabled: false // Disable swipe gesture
+      }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="movie/[id]" />
       </Stack>
     </>
   )
